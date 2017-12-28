5120cfa75419cdd28c6912b21c1af61c145f22c7
Едиге Нуркайыр Маратулы <@corp.itstep>
2017-12-28 11:36:17 +0600

    homework

---
+++ GCHW/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            GC.GetGeneration(person);
+            GC.Collect();
+            Console.WriteLine(GC.GetGeneration(person));
+            Console.ReadLine();
+        }
+    }
+}
+
